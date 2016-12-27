d7be2b5a4137d3c690aeb247a87f569fd9374095
digital planets <digplan@users.noreply.github.com>
2016-12-27 11:07:01 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var gsheet = require('gsheet-web');
-gsheet.get('1KPJr7wBEzicgrqukhEZBdGCqNQQB-ldrRrS0lxmq_qo', function(data){
+gsheet('1KPJr7wBEzicgrqukhEZBdGCqNQQB-ldrRrS0lxmq_qo', function(data){
   console.log(data);  // array of objects
 });
